ce6a37da6d468794939a0ffdc4ca855bbb7e8409
TJhands <taiquan_yuan@foxmail.com>
2020-03-20 22:19:48 +0000

    database_info

--- db_config.py
+++ db_config.py
@@ -3,15 +3,9 @@ import setting
 
 
 
-ENGINE_PROJECT_ENGLISH = create_engine('mysql+pymysql://{0}:{1}@{2}:{3}/{4}'.format(setting.MYSQL_USER,
+ENGINE_ADS_COURSEWORK = create_engine('mysql+pymysql://{0}:{1}@{2}:{3}/{4}'.format(setting.MYSQL_USER,
                                                                            setting.MYSQL_PASSWD,
                                                                            setting.MYSQL_HOST_ENGLISH,
                                                                            setting.MYSQL_PORT,
-                                                                           'project'),
-                              connect_args={'charset': 'utf8'}, pool_size=8)
-ENGINE_PROJECT_RUSSIAN = create_engine('mysql+pymysql://{0}:{1}@{2}:{3}/{4}'.format(setting.MYSQL_USER,
-                                                                           setting.MYSQL_PASSWD,
-                                                                           setting.MYSQL_HOST_RUSSIAN,
-                                                                           setting.MYSQL_PORT,
-                                                                           'project'),
+                                                                           'ads_coursework'),
                               connect_args={'charset': 'utf8'}, pool_size=8)
--- sqlpkg.py
+++ sqlpkg.py
@@ -1,7 +1,4 @@
 
-sql_test = "SELECT postID,flatContent FROM exetools_post LIMIT 1"
-sql_postdate = "SELECT postDate,flatContent FROM exetools_post where authorName != '' and authorName is not NULL "
-sql_user_time = "SELECT postID,authorName,postDate FROM exetools_post WHERE authorName != '' and authorName is not NULL "
 def get_data(table):
     sql = "SELECT authorName,postDate,flatContent FROM {} where authorName != '' and authorName is not NULL and flatContent != '' and flatContent is not NULL order by authorName,postDate".format(table)
     return sql
