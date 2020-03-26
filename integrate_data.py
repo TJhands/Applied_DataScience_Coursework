373c4159f5f4dc683f3a9fd74023ed144da72eaf
TJhands <taiquan_yuan@foxmail.com>
2020-03-26 20:40:36 +0000

    homelessness_england,tosql,area_code

--- integrate_data.py
+++ integrate_data.py
@@ -3,6 +3,7 @@ import numpy as np
 import db_config
 from tosql import to_sql
 import sqlpkg
+import arrow
 ENGINE_ADS = db_config.ENGINE_ADS_COURSEWORK
 
 
@@ -29,18 +30,18 @@ def store_homelessness_england():
     to csv + mysql
     :return:
     """
-    df = pd.read_excel("../data/DetailedLA_201909.xlsx", sheet_name="MD1")
-    df = df.iloc[:, [0,5, 6, 7]].dropna().reset_index(drop=True)
+    df = pd.read_excel("../data/homelessness/DetailedLA_201806.xlsx", sheet_name="MD1 - Apr-Jun 18")
+    df = df.iloc[:, [0, 5, 6, 7]].dropna().reset_index(drop=True)
     df.columns = ["area_code", 'N1','N2','N3']
     df['feature_name'] = 'homelessness'
-    df['quarter'] = 'Q3'
-    df['year'] = '2019'
+    df['quarter'] = 'Q2'
+    df['year'] = '2018'
     df['feature_value'] = df.N1+df.N2+df.N3
     df.replace("......",'None',inplace = True)
     df.drop(['N1','N2','N3'],axis = 1,inplace = True)
     to_sql("features", ENGINE_ADS, df, type="update", chunksize=2000)
-    df = df.groupby(['area_code','year','quarter','feature_name'])['feature_value'].last().unstack(level=3).reset_index()
-    df.to_csv("./data/homeless_england.csv", index=False)
+    # df = df.groupby(['area_code','year','quarter','feature_name'])['feature_value'].last().unstack(level=3).reset_index()
+    # df.to_csv("./data/homeless_england.csv", index=False)
     return
 
 def store_hpi_sales():
@@ -53,22 +54,26 @@ def store_hpi_sales():
     area_code = pd.read_sql(sql,ENGINE_ADS,index_col='area_code')
     indices = pd.read_csv("../data/HPI/Indices-2019-12.csv").drop('Region_Name',axis=1)
     sales = pd.read_csv("../data/HPI/Sales-2019-12.csv").drop('Region_Name', axis=1)
-    indices = indices.loc[indices['Date'].apply(lambda x: True if x >= '2019-07-01' and x <= '2019-09-01' else False)]
-    sales = sales.loc[sales['Date'].apply(lambda x: True if x >= '2019-07-01' and x <= '2019-09-01' else False)]
-    indices = indices.groupby('Area_Code').mean()
-    sales = sales.groupby('Area_Code').mean()
-    result = area_code.join(indices).drop('area_name',axis = 1)
-    result['year'] = '2019'
-    result['quarter'] = 'Q3'
-    result['feature_name'] = 'hpi'
-    result = result.rename({'Index':'feature_value'},axis = 1).reset_index()
-    result_sales = area_code.join(sales).drop('area_name', axis = 1)
-    result_sales['year'] = '2019'
-    result_sales['quarter'] = 'Q3'
-    result_sales['feature_name'] = 'sales_volume'
-    result_sales = result_sales.rename({'Sales_Volume': 'feature_value'},axis = 1).reset_index()
-    to_sql("features", ENGINE_ADS, result, type="update", chunksize=2000)
-    to_sql("features", ENGINE_ADS, result_sales, type="update", chunksize=2000)
+    # indices.Date = indices.Date.apply(lambda x: arrow.get(x).date())
+    # sales.Date = sales.Date.apply(lambda x: arrow.get(x).date())
+    for year in range(2013,2019):
+        for quarter in range(1,5):
+            indices_ = indices.loc[indices['Date'].apply(lambda x: True if x >= arrow.get('{}-{}-01'.format(year,1+(quarter-1)*3)).format("YYYY-MM-DD") and x <= arrow.get('{}-{}-01'.format(year,3+(quarter-1)*3)).format("YYYY-MM-DD") else False)]
+            sales_ = sales.loc[sales['Date'].apply(lambda x: True if x >= arrow.get('{}-{}-01'.format(year,1+(quarter-1)*3)).format("YYYY-MM-DD") and x <= arrow.get('{}-{}-01'.format(year,3+(quarter-1)*3)).format("YYYY-MM-DD") else False)]
+            indices_ = indices_.groupby('Area_Code').mean()
+            sales_ = sales_.groupby('Area_Code').mean()
+            result = area_code.join(indices_).drop('area_name',axis = 1)
+            result['year'] = '{}'.format(year)
+            result['quarter'] = 'Q{}'.format(quarter)
+            result['feature_name'] = 'hpi'
+            result = result.rename({'Index':'feature_value'},axis = 1).reset_index()
+            result_sales = area_code.join(sales_).drop('area_name', axis = 1)
+            result_sales['year'] = '{}'.format(year)
+            result_sales['quarter'] = 'Q{}'.format(quarter)
+            result_sales['feature_name'] = 'sales_volume'
+            result_sales = result_sales.rename({'Sales_Volume': 'feature_value'},axis = 1).reset_index()
+            to_sql("features", ENGINE_ADS, result, type="update", chunksize=2000)
+            to_sql("features", ENGINE_ADS, result_sales, type="update", chunksize=2000)
     return
 
 def get_feature_data():
@@ -82,4 +87,4 @@ def get_feature_data():
         level=3).reset_index()
     return
 if __name__ == '__main__':
-    get_feature_data()
+    store_homelessness_england()
