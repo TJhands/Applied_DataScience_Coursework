3e9849fd3c9da1ab37a4ae0bb311db7adf56e6a7
qin <0416602cw@gmail.com>
2020-04-01 23:43:42 +0100

    draw data-1

--- draw_multi.py
+++ draw_multi.py
@@ -14,34 +14,36 @@ from  integrate_data import get_feature_data
 import seaborn as sns
 import statsmodels.formula.api as smf
 
-ENGINE_ADS = db_config.ENGINE_ADS_COURSEWORK
+
 
 pd.set_option('display.max_columns', 500)
-sql = sqlpkg.get_features()
-features = pd.read_sql(sql, ENGINE_ADS)
-features = features.groupby(['area_code', 'year', 'quarter', 'feature_name'])['feature_value'].last().unstack(
-        level=3).reset_index()
-features=features.dropna()
-print((features))
+
+
+features=get_feature_data()
 print(features.describe())
-features=features[features["sales_volume"]<554.72]
-features=features[features["homelessness"]<104.5]
-features=features[features['new_dwelling_complete']<290]
-#features=features[features['PrivateEnterprise_complete']<230]
-#features=features[features['HousingAssociations_complete']<75]
+# features=features[features["homelessness"]<104.5]
+
 df=features
-print(features.columns)
+df= features[['homelessness','new_dwelling_start','new_dwelling_complete','hpi','sales_volume']]
+# print(features.columns)
 homeless =features["homelessness"]
 coo= df.corrwith(homeless)
 print(coo)
 
+# df.plot.scatter(x='hpi', y='sales_volume')
 
-df=df[["homelessness",'hpi','sales_volume','new_dwelling_complete','PrivateEnterprise_complete','LocalAuthority_complete','HousingAssociations_complete']]
-#df.plot.scatter(x='hpi', y='sales_volume')
 
 Train,Test = train_test_split(df, train_size = 0.8, random_state=1234)
-fit = smf.ols('homelessness~sales_volume', data = Train).fit()
+#y=homelessness
+#x=sales_volume+hpi
+
+fit = smf.ols('homelessness~sales_volume+hpi+hpi+new_dwelling_start+new_dwelling_complete', data = Train).fit()
 print(fit.summary())
 
-sns.pairplot(df)
+
+# x=features["homelessness"]
+# h=x.hist(bins=100)
 # plt.show()
+
+sns.pairplot(df)
+#plt.show()
