ce3e1d02153bd73044ce6ad5a3d797e8979ce828
qin <0416602cw@gmail.com>
2020-04-12 22:27:21 +0100

    draw data-1

--- draw_mult_01i.py
+++ draw_mult_01i.py
@@ -56,7 +56,7 @@ hm = sns.heatmap(cm,
                  square=True,
                  fmt='.2f',
                  annot_kws={'size':15},
-                 yticklabels=['homelessness','nds','ndc','hpi','sv'],
-                 xticklabels=['homelessness','nds','ndc','hpi','sv'])
+                 yticklabels=['hl','nds','ndc','hpi','sv'],
+                 xticklabels=['hl','nds','ndc','hpi','sv'])
 plt.show()
 
