19c814e44d2a8bd21b084c84e3cdd37e7bc0fb39
TJhands <taiquan_yuan@foxmail.com>
2020-05-01 18:07:31 +0800

    new database

--- setting.py
+++ setting.py
@@ -10,4 +10,4 @@ MYSQL_PORT = 3306
 # MYSQL_HOST_RUSSIAN = "35.173.190.93"
 MYSQL_HOST_CHINA = 'rm-bp1f09qn5y3h3smvako.mysql.rds.aliyuncs.com'
 MYSQL_PASSWD_CHINA = 'sv7tgB0mo2pzboRa'
-MYSQL_USER_CHINA = "universe"
+MYSQL_USER_CHINA = "common"
